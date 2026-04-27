20fd910c038f639ed8fc9ceeb07459d30ba5db7e
Ernest V <ernest.viol@gmail.com>
2026-04-27 13:24:45 -0700

    feat: Add form validation
    Added form validation with express-validator. Did not style the form
    errors because I'm ready to move on.

--- controllers/newMessageController.js
+++ controllers/newMessageController.js
@@ -1,20 +1,38 @@
+import { body, validationResult, matchedData } from 'express-validator';
 import { newMessage } from '../db/queries.js';
+
+const validateMessage = [
+  body('username')
+    .trim()
+    .isAlphanumeric()
+    .isLength({ min: 1, max: 255 })
+    .withMessage('Must be a username under 255 characters'),
+  body('message'),
+];
 
 const newMessageController = {
   get: (req, res) => {
     res.render('newMessage');
   },
 
-  post: async (req, res) => {
-    try {
-      const { username, message } = req.body;
-      await newMessage(username, message);
-      res.redirect('/');
-    } catch (error) {
-      console.log(error);
-      res.status(500).send('Internal Error');
-    }
-  },
+  post: [
+    validateMessage,
+    async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).render('newMessage', { errors: errors.array() });
+      }
+
+      try {
+        const { username, message } = matchedData(req);
+        await newMessage(username, message);
+        res.redirect('/');
+      } catch (error) {
+        console.log(error);
+        res.status(500).send('Internal Error');
+      }
+    },
+  ],
 };
 
 export default newMessageController;
