ab6040c5cdc5cd77cdeb4d5bd410cbc30ee79249
Ernest V <ernest.viol@gmail.com>
2026-04-27 15:22:37 -0700

    feat: Add example environment variables

--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,22 @@
 import { Pool } from 'pg';
 
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+const isCloudRun = !!process.env.K_SERVICE;
+
+const pool = new Pool(
+  isCloudRun
+    ? {
+        user: process.env.DB_USER,
+        database: process.env.DB_DATABASE,
+        password: process.env.DB_PASSWORD,
+        host: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
+      }
+    : {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        database: process.env.DB_DATABASE,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT,
+      },
+);
 
 export default pool;
